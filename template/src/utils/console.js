d4a117c1cb3033997c608b99e8737d494523b736
张雄军 <zxj@blibao.cn>
2020-04-10 00:37:13 +0800

    perf: 细节优化

--- template/src/utils/console.js
+++ template/src/utils/console.js
@@ -7,20 +7,15 @@
 import Vue from 'vue';
 import VConsole from 'vconsole';
 
-const DEVELOP_ENV = process.env.VUE_APP_ENV === 'development';
-const RELEASE_ENV = process.env.VUE_APP_ENV === 'release';
-
 // 开发环境、正式环境默认不开启调试工具
-if (!DEVELOP_ENV && !RELEASE_ENV) {
-  window.vConsole = new VConsole();
+window.vConsole = new VConsole();
 
-  // window 错误
-  window.addEventListener('error', (error) => {
-    console.error(error);
-  }, true);
+// window 错误
+window.addEventListener('error', (error) => {
+  console.error(error);
+}, true);
 
-  // VUE 错误
-  Vue.config.errorHandler = (error, vm, info) => {
-    console.error(error);
-  };
-}
+// VUE 错误
+Vue.config.errorHandler = (error, vm, info) => {
+  console.error(error);
+};
